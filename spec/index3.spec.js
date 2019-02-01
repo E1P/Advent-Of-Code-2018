2f63864c25d162ef3711fdf73cba72d2599ed150
E1P <ewan.pearce@gmail.com>
2019-02-01 13:20:50 +0000

    Add first test

---
+++ spec/index3.spec.js
@@ -0,0 +1,8 @@
+const { expect } = require('chai');
+const overlapFinder = require('../index3');
+
+describe('OverlapFinder()', () => {
+  it('should be a function', () => {
+    expect(overlapFinder).to.be.a('function');
+  })
+})
