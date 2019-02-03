e055452abb960430dfdf010d980cd5f354b207a5
E1P <ewan.pearce@gmail.com>
2019-02-03 13:31:05 +0000

    Add working hourTally function and tests

--- day4/index4.js
+++ day4/index4.js
@@ -8,9 +8,43 @@ const chronologise = input => {
   })
 };
 
-const orderedLogs = chronologise(data);
-console.log(orderedLogs);
+const hourTally = logArr => {
+  let guard;
+  let minute = 0;
+  let asleep = false;
+  const guardZZs = {};
+  logArr.forEach((logItem, index) => {
+    if (/Guard/.test(logItem)) {
+      minute = 0;
+      asleep = false;
+      guard = logItem.match(/(?<=#)\d+/)[0];
+      if (!guardZZs[guard]) guardZZs[guard]= {};
+    } else {
+      for (let t = minute; t < 60 && t < +logItem.match(/(?<=:)\d+/)[0]; t++) {
+        if (!asleep) guardZZs[guard][t] = (guardZZs[guard][t] || 0);
+        else guardZZs[guard][t] = (guardZZs[guard][t] || 0) + 1;
+        console.log(index, minute, guardZZs[guard][t]);
+        minute++;
+      }
+      asleep = !asleep;
+      console.log('Time of sleep change', logItem.match(/(?<=:)\d+/)[0])
+      console.log('Guard asleep? > ', asleep);
+      if (!logArr[index+1] || /Guard/.test(logArr[index+1])) {
+        for (minute; minute < 60; minute++) {
+          guardZZs[guard][minute] = (guardZZs[guard][minute] || 0);
+          console.log(index, minute, guardZZs[guard][minute]);
+        };
+      }; 
+    }
+    console.log('minute at end of each item >>> ', minute)
+  })
+  console.log(guardZZs);
+  return guardZZs;
+};
 
-module.exports = { chronologise };
+const chronoLogs = chronologise(data);
+console.log(hourTally(chronoLogs));
+
+module.exports = { chronologise, chronoLogs, hourTally };
 
 // [1518-06-12 00:00] Guard #3359 begins shift
--- spec/index4.spec.js
+++ spec/index4.spec.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const { chronologise } = require('../day4/index4.js');
+const { chronologise, chronoLogs, hourTally } = require('../day4/index4.js');
 
 describe.only('Day 4', () => {
   describe('chronologise()', () => {
@@ -12,6 +12,15 @@ describe.only('Day 4', () => {
       const guardLogs = "[1518-03-22 00:45] falls asleep\n[1518-03-22 00:52] wakes up\n[1518-03-22 23:59] Guard #277 begins shift\n[1518-03-22 00:00] Guard #1231 begins shift";
       const orderedLogs = ['[1518-03-22 00:00] Guard #1231 begins shift', '[1518-03-22 00:45] falls asleep', '[1518-03-22 00:52] wakes up', '[1518-03-22 23:59] Guard #277 begins shift'];
       expect(chronologise(guardLogs)).to.eql(orderedLogs);
+    });
+  });
+  describe('hourTally()', () => {
+    it('should tally sleep data for a single guard\'s shift, with "0" for awake and "1" for asleep', () => {
+      console.log('Sliced logs: ', chronoLogs.slice(0, 7));
+      const guard1201mins = {0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:1,15:1,16:1,17:1,18:1,19:1,20:1,21:1,22:1,23:1,24:1,25:1,26:1,27:1,28:1,29:1,30:0,31:0,32:0,33:0,34:0,35:0,36:0,37:1,38:1,39:1,40:1,41:1,42:0,43:0,44:0,45:0,46:0,47:0,48:0,49:0,50:1,51:1,52:1,53:0,54:0,55:0,56:0,57:0,58:0,59:0};
+      expect(hourTally(chronoLogs.slice(0, 7))).to.be.an('object');
+      expect(hourTally(chronoLogs.slice(0, 1))).to.eql({1201: {}});
+      expect(hourTally(chronoLogs.slice(0, 7))).to.eql({1201: guard1201mins});
     });
   });
 });
