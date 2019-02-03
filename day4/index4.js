6f5b588aa3dbf9127f6fab3aced03cad61a3848f
E1P <ewan.pearce@gmail.com>
2019-02-03 10:46:35 +0000

    Refactor chronologise with better regex and joins to avoid multiple 'if' statements.

--- day4/index4.js
+++ day4/index4.js
@@ -2,18 +2,9 @@ const data = require('./day4-inputs.json');
 
 const chronologise = input => {
   return input.split(/\n/g).sort((a, b) => {
-    const aTime = a.match(/\d+/g);
-    const bTime = b.match(/\d+/g);
-      // *Time[1] = month, [2] = date, [3] = hour, [4] = minute;
-    if (aTime[1] !== bTime[1]) {
-      return aTime[1] - bTime[1];
-    } else if (aTime[2] !== bTime[2]) {
-      return aTime[2] - bTime[2];
-    } else if (aTime[3] !== bTime[3]) {
-      return aTime[3] - bTime[3];
-    } else {
-      return aTime[4] - bTime[4];
-    }
+    const aTime = a.match(/(?<=-)\d+|(?<=\s)\d+|(?<=:)\d+/g).join('');
+    const bTime = b.match(/(?<=-)\d+|(?<=\s)\d+|(?<=:)\d+/g).join('');
+    return aTime - bTime;
   })
 };
 
@@ -21,3 +12,5 @@ const orderedLogs = chronologise(data);
 console.log(orderedLogs);
 
 module.exports = { chronologise };
+
+// [1518-06-12 00:00] Guard #3359 begins shift
